9f393def1ccf19f93add011121f35ed3b27a7cba
happyprogrammer-code <reksbark699@gmail.com>
2020-12-31 03:18:13 +0700

    hari pertama

--- src/sections/Landing.js
+++ src/sections/Landing.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Landing.css'
-import {smallLanding, bigLanding, LogoDark, LogoLight, burger} from './img'
+import {smallLanding, bigLanding, LogoDark, LogoLight, burger, mainTv} from './img'
 export default function Landing() {
+
+    const [dropdown, setDropdownState] = useState(false)
+
+    const dropdownState = dropdown === true ? 'nav-dropdown nav-dropdown-active' : 'nav-dropdown'
+
+    function setDropdown(){
+        setDropdownState(!dropdown)
+    }
+    
     return (
         <section className="landing-main">
             <div className="landing-topper-small">
@@ -28,12 +37,52 @@ export default function Landing() {
                                 </ul>
                             </div>
                             <div className="nav-burger-logo">
-                                <button><img src={burger} alt="" srcset=""/></button>
+                                <button onClick={setDropdown}><img src={burger} alt="" srcset=""/></button>
                             </div>
                         </div>
                    </div>
                 </div>
+             
             </nav>
+            <div className="landing-big-area">
+                <div className="con-90-res">
+                    <div className="landing-big-in">
+                        <div className="landing-image">
+                            <img src={mainTv} alt="TV"/>
+                        </div>
+                        <div className="landing-texts">
+                            <div className="landing-texts-in">
+                            <h1>Design your own televison</h1>
+                            <p>Design you own Television design your own experience, design your own future experience, design your own future</p>
+                            <input type="button" value="GET STARTED"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={dropdownState}>
+                <div className="link-list">
+                        <a href="#"><div className="links">
+                            <p>Home</p>
+                        </div>
+                        </a>
+                        <a href="#">
+                        <div className="links">
+                            <p>About</p>
+                        </div>
+                        </a>
+                        <a href="#">
+                        <div className="links">
+                            <p>Contact</p>
+                        </div>
+                        </a>
+                        <a href="#">
+                        <div className="links">
+                            <p>FAQ</p>
+                        </div>
+                        </a>
+                    </div>
+                </div>
             <div className="con-90-res">
                 <h1>lololS</h1>
             </div>
